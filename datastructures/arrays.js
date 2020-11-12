94e3a338d0d4a7561de3a405ec50bba39682cbbf
Jonathan <jonathan.george.dev@gmail.com>
2020-11-11 23:50:31 -0800

    study time 2

--- datastructures/arrays.js
+++ datastructures/arrays.js
@@ -338,3 +338,140 @@ module.exports = LinkedList;
 /////
 /////
 //printing a list of stuff
+const Node = require('./Node');
+
+class LinkedList {
+  
+  constructor() {
+    this.head = null;
+  }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    this.head = newHead;
+    if (currentHead) {
+      this.head.setNextNode(currentHead);
+    }
+  }
+
+  addToTail(data) {
+    let tail = this.head;
+    if (!tail) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() !== null) {
+        tail = tail.getNextNode();
+      }
+      tail.setNextNode(new Node(data));
+    }
+  }
+
+  removeHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    return removedHead.data;
+  }
+
+  printList() {
+    let currentNode = this.head;
+    let output = '<head> ';
+    while (currentNode !== null) {
+      output += currentNode.data + ' ';
+      currentNode = currentNode.getNextNode();
+    }
+    output += '<tail>';
+    console.log(output);
+  }
+
+}
+
+module.exports = LinkedList;
+
+
+
+
+
+
+
+
+
+////////////////////////
+////////////////////////
+//Using the Linked List
+//ues the code above
+//and a node.js file 
+
+
+const LinkedList = require('./LinkedList');
+
+const seasons = new LinkedList();
+seasons.printList();
+
+seasons.addToHead('summer');
+seasons.addToHead('spring');
+seasons.printList();
+
+seasons.addToTail('fall');
+seasons.addToTail('winter');
+seasons.printList();
+
+seasons.removeHead();
+seasons.printList();
+
+
+//node.js file
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  
+    setNextNode(node) {
+      if (!(node instanceof Node)) {
+        throw new Error('Next node must be a member of the Node class');
+      }
+      this.next = node;
+    }
+  
+    getNextNode() {
+      return this.next;
+    }
+  }
+  
+  module.exports = Node;
+
+
+
+
+
+  //////
+  ////////
+  //what will this output?
+
+  class LinkedList {
+    constructor() {
+      this.head = null;
+    }
+    
+    addToHead(data) {/* Method that adds a node to the head of the list */}
+    
+    addToTail(data) {/* Method that adds a node to the tail of the list */}
+    
+    removeHead() {/* Method that removes the head of the list */}
+  }
+   
+  const testList = new LinkedList();
+  testList.addToHead(3);
+  testList.addToHead(8);
+  testList.addToTail(2);
+  testList.removeHead();
+  testList.addToTail(4);
+  testList.removeHead();
+  console.log(testList.head.data);
+
+
+  //2
